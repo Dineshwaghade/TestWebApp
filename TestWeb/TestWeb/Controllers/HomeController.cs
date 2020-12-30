5a178c6f52ee0eed814cf7cce9ef3970b117609e
Dinesh waghade <dineshwaghade12@gmail.com>
2020-12-30 20:35:00 +0530

    tes

---
+++ TestWeb/TestWeb/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestWeb.Models;
+
+namespace TestWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext db = new DataContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Users.ToList();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User usr)
+        {
+            db.Users.Add(usr);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ TestWeb/TestWeb/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestWeb.Models
+{
+    public class User
+    {
+
+        [Key]
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Gender{ get; set; }
+        public string Address{ get; set; }
+    }
+}
